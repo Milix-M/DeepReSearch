bdc5ed467024704c60fea365430d251bfed88835
Milix-M <70957923+Milix-M@users.noreply.github.com>
2025-10-30 22:03:00 +0900

    DeepResearchAIを実装

---
+++ src/ai/search/deep_research.py
@@ -0,0 +1,28 @@
+from ..schedule.plan_reserch import GeneratedObjectSchema
+from .prompt import DEEP_RESEARCH_SYSTEM_PROMPT
+
+
+class DeepResearchAI:
+    def __init__(self, llm):
+        self.llm = llm
+
+    def __call__(
+        self,
+        search_queries_per_section: int,
+        search_iterations: int,
+        search_plan: GeneratedObjectSchema,
+    ):
+        prompt = [
+            (
+                "system",
+                DEEP_RESEARCH_SYSTEM_PROMPT.format(
+                    SEARCH_QUERIES_PER_SECTION=search_queries_per_section,
+                    SEARCH_API="DuckDuckGo",
+                    SEARCH_ITERATIONS=search_iterations,
+                    SEARCH_PLAN=search_plan,
+                ),
+            )
+        ]
+        response = self.llm.invoke(prompt)
+
+        return response
