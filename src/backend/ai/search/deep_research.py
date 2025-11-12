5fb4a143f0d0312743c15c61300deeff6ed0d0a7
K.T <70957923+Milix-M@users.noreply.github.com>
2025-11-12 23:15:32 +0900

    get_current_dateをプロンプトに統合

--- src/backend/ai/search/deep_research.py
+++ src/backend/ai/search/deep_research.py
@@ -1,3 +1,4 @@
+import datetime
 from ..schedule.plan_reserch import GeneratedObjectSchema
 from .prompt import DEEP_RESEARCH_SYSTEM_PROMPT
 
@@ -20,6 +21,7 @@ class DeepResearchAI:
                     SEARCH_API="DuckDuckGo",
                     SEARCH_ITERATIONS=search_iterations,
                     SEARCH_PLAN=search_plan,
+                    CURRENT_DATE=datetime.date.today(),
                 ),
             )
         ]
